2d34eb35e653779e2bcc1bcb3592bff2b08956a2
Brannigan Sakwah <brannigansakwah@gmail.com>
2023-03-19 04:05:47 +0300

    ++builtin conversions

--- src/intepreter/builtins.rs
+++ src/intepreter/builtins.rs
@@ -1,6 +1,6 @@
 use text_io::read;
 
-use crate::parser::ast::Identifier;
+use crate::{intepreter::Evaluator, parser::ast::Identifier};
 
 use super::obj::{BuiltinFunction, Object};
 
@@ -22,6 +22,8 @@ impl BuiltinsFunctions {
             add_builtin("andika", 1, bprint_fn),
             add_builtin("soma", 1, bread_fn),
             add_builtin("soma_namba", 1, bread_num_fn),
+            add_builtin("bulin", 1, b2bool_fn),
+            add_builtin("namba", 1, b2num_fn),
             add_builtin("urefu", 1, blen_fn),
         ]
     }
@@ -37,12 +39,8 @@ fn add_builtin(name: &str, param_num: usize, func: BuiltinFunction) -> (Identifi
 
 fn bprint_fn(args: Vec<Object>) -> Result<Object, String> {
     match args.get(0) {
-        Some(Object::String(t)) => {
-            println!("{}", t);
-            Ok(Object::Null)
-        }
         Some(o) => {
-            println!("{}", o);
+            println!("{}", Evaluator::ots(o.clone()));
             Ok(Object::Null)
         }
         _ => Err(String::from("")), // TODO error message
@@ -51,13 +49,8 @@ fn bprint_fn(args: Vec<Object>) -> Result<Object, String> {
 
 fn bread_fn(args: Vec<Object>) -> Result<Object, String> {
     match args.get(0) {
-        Some(Object::String(t)) => {
-            println!("{}", t);
-            let input: String = read!("{}\n");
-            Ok(Object::String(input))
-        }
         Some(o) => {
-            println!("{}", o);
+            println!("{}", Evaluator::ots(o.clone()));
             let input: String = read!("{}\n");
             Ok(Object::String(input))
         }
@@ -67,13 +60,8 @@ fn bread_fn(args: Vec<Object>) -> Result<Object, String> {
 
 fn bread_num_fn(args: Vec<Object>) -> Result<Object, String> {
     match args.get(0) {
-        Some(Object::String(t)) => {
-            println!("{}", t);
-            let input: f64 = read!("{}\n");
-            Ok(Object::Number(input))
-        }
         Some(o) => {
-            println!("{}", o);
+            println!("{}", Evaluator::ots(o.clone()));
             let input: f64 = read!("{}\n");
             Ok(Object::Number(input))
         }
@@ -81,6 +69,23 @@ fn bread_num_fn(args: Vec<Object>) -> Result<Object, String> {
     }
 }
 
+fn b2bool_fn(args: Vec<Object>) -> Result<Object, String> {
+    match args.get(0) {
+        Some(o) => Evaluator::otb(o.clone())
+            .map(Object::Boolean)
+            .map_err(|_| String::from("Conversion to boolean failed")),
+        _ => Err(String::from("")), // TODO error message
+    }
+}
+
+fn b2num_fn(args: Vec<Object>) -> Result<Object, String> {
+    match args.get(0) {
+        Some(o) => Evaluator::otn(o.clone())
+            .map(Object::Number)
+            .map_err(|_| String::from("Conversion to number failed")),
+        _ => Err(String::from("")), // TODO error message
+    }
+}
 
 fn blen_fn(args: Vec<Object>) -> Result<Object, String> {
     match args.get(0) {
